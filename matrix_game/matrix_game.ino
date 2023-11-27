134aebf4143503e2e4f92d56cc46d5d18d6a53a3
ilinca-ana-moraru <ilincaanamoraru@gmail.com>
2023-11-27 13:29:27 +0200

    added photo

--- matrix_game/matrix_game.ino
+++ matrix_game/matrix_game.ino
@@ -1,16 +1,14 @@
 #include "LedControl.h" // Include LedControl library for controlling the LED matrix
-#include "time.h"
-const int dinPin = 12;
-const int clockPin = 11;
-const int loadPin = 10;
+#define DIN_PIN  12
+#define CLOCK_PIN  11
+#define LOAD_PIN  10
 
-const int pressPin = 13;
-const int xPin = A0;
-const int yPin = A1;
-// Create an LedControl object to manage the LED matrix
-LedControl lc = LedControl(dinPin, clockPin, loadPin, 1); // DIN, CLK, LOAD, No. DRIVER
-// Variable to set the brightness level of the matrix
-byte matrixBrightness = 2;
+#define PRESS_PIN 13
+#define X_PIN A0
+#define Y_PIN  A1
+LedControl lc = LedControl(DIN_PIN, CLOCK_PIN, LOAD_PIN, 1); // DIN, CLK, LOAD, No. DRIVER
+
+#define MATRIX_BRIGHTNESS  15
 
 #define EMPTY_SPACE 0
 #define PLAYER 1
@@ -18,31 +16,30 @@ byte matrixBrightness = 2;
 #define WALL 3
 
 #define DROPPED_BOMB 4
-#define SHOWN_BOMB 5
-#define EXPANDED_BOMB 6
-// Variables to track the current and previous positions of the joystick-controlled LED
-// Thresholds for detecting joystick movement
-const int minThreshold = 312;
-const int maxThreshold = 712;
+#define EXPAND_BOMB 5
+#define DELETE_BOMB 6
+
+#define MIN_THRESHOLD  312
+#define MAX_THRESHOLD 712
 #define DEFAULT_JOYSTICK 512
-const byte moveInterval = 200; // Timing variable to control the speed of LED movement
-unsigned long long lastMoved = 0; // Tracks the last time the LED moved
+#define MOVE_INTERVAL 200 
+unsigned long long lastMoved = 0; 
 
-const byte matrixSize = 8 ;// Size of the LED matrix
-const int mapSize = 16;
+#define MATRIX_SIZE 8 
+#define MAP_SIZE 16
 
-int xBias = matrixSize/2;
-int yBias = matrixSize/2;
-byte xDefaultDistanceBetweenPosAndBias = matrixSize/2;
-byte yDefaultDistanceBetweenPosAndBias = matrixSize/2;
+short xBias = MATRIX_SIZE/2;
+short yBias = MATRIX_SIZE/2;
+byte xDefaultDistanceBetweenPosAndBias = MATRIX_SIZE/2;
+byte yDefaultDistanceBetweenPosAndBias = MATRIX_SIZE/2;
 byte xPos = xDefaultDistanceBetweenPosAndBias + xBias;
 byte yPos = yDefaultDistanceBetweenPosAndBias + yBias;
 byte xLastPos = 0;
 byte yLastPos = 0;
 
-bool matrixChanged = true; // Flag to track if the matrix display needs updating
-// 2D array representing the state of each LED (on/off) in the matrix
-byte matrix[mapSize][mapSize] = {
+bool matrixChanged = true; 
+// 2D array representing what is on the map currently (wall/bomb)
+byte matrix[MAP_SIZE][MAP_SIZE] = {
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -65,7 +62,6 @@ bool playerBlinkingState = 1;
 unsigned long playerLastBlink = 0;
 #define PLAYER_BLINK_INTERVAL  100
 
-//bomb variables
 struct bomb{
   byte x;
   byte y;
@@ -74,11 +70,9 @@ struct bomb{
 };
 bomb* bombs;
 
-int nrOfBombs = 0;
-bool lastPressState = 0;
+byte nrOfBombs = 0;
 bool pressState = 0;
 bool pressRead = 0;
-bool lastPressRead = 0;
 unsigned long lastPressCheck = 0;
 #define PRESS_DEBOUNCE_TIME 50
 
@@ -94,14 +88,14 @@ struct direction{
   int x;
   int y;
 };
-byte nrOfDirections;
+#define NR_OF_DIRECTIONS 4
                             // up     down    left    right
-direction directions[4] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-const int nrOfExtendedBombCoordones = 13;
-direction extendedBombCoordonates[nrOfExtendedBombCoordones] = {{-2, 0}, {-1, -1},{-1, 0}, {-1, 1},
+direction directions[NR_OF_DIRECTIONS] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+#define NR_OF_EXTENDED_BOMB_COORDONATES 13
+direction extendedBombCoordonates[NR_OF_EXTENDED_BOMB_COORDONATES] = {{-2, 0}, {-1, -1},{-1, 0}, {-1, 1},
 {0, -2}, {0, -1}, {0, 1}, {0, 2}, {1, -1}, {1, 0}, {1, 1}, {2, 0}, {0, 0}};
 
-byte skullAnimation[matrixSize][matrixSize] = {
+bool skullAnimation[MATRIX_SIZE][MATRIX_SIZE] = {
   {0, 1, 1, 1, 1, 1, 1, 0},
   {1, 1, 1, 1, 1, 1, 1, 1},
   {1, 0, 0, 1, 1, 0, 0, 1},
@@ -111,13 +105,11 @@ byte skullAnimation[matrixSize][matrixSize] = {
   {0, 1, 0, 0, 0, 0, 1, 0},
   {0, 0, 1, 1, 1, 1, 0, 0}  
 };
-bool displayDeath = 0;
-bool displayWin = 0;
 unsigned long showDeathStart = 0;
 unsigned long showWinStart = 0;
 #define DEFAULT_ANIMATION_DISPLAY 2000
 
-byte winAnimation[matrixSize][matrixSize] = {
+bool winAnimation[MATRIX_SIZE][MATRIX_SIZE] = {
   {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 1, 1, 0, 0, 1, 1, 0},
   {0, 1, 1, 0, 0, 1, 1, 0},
@@ -127,35 +119,46 @@ byte winAnimation[matrixSize][matrixSize] = {
   {0, 0, 1, 1, 1, 1, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0}  
 };
-
+#define PLAYING 0
+#define DEATH_ANIMATION 1
+#define WINNING_ANIMATION 2
+byte gameState = PLAYING;
 
 void setup() {
 
-  pinMode(pressPin, INPUT_PULLUP);
+  pinMode(PRESS_PIN, INPUT_PULLUP);
+  pinMode(X_PIN, INPUT);
+  pinMode(Y_PIN, INPUT);
+  pinMode(DIN_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(LOAD_PIN, OUTPUT);
   Serial.begin(115200);
   lc.shutdown(0, false); 
-  lc.setIntensity(0, matrixBrightness); 
+  lc.setIntensity(0, MATRIX_BRIGHTNESS); 
   lc.clearDisplay(0);
-  randomSeed(analogRead(pressPin));
+  randomSeed(analogRead(PRESS_PIN));
   generateMap();
   updateMatrix();
 }
 void loop() {
 
 
-  // masurarea joystickului
-  if (millis() - lastMoved > moveInterval) { 
-    updatePositions(); 
-    lastMoved = millis(); 
+  //check if the joystick moved
+  if(gameState == PLAYING){
+    if (millis() - lastMoved > MOVE_INTERVAL) { 
+      updatePositions(); 
+      lastMoved = millis(); 
+    }
+
   }
-  // schimbarea matrixului si afisarea corespunzatoare
+  // change matrix if necessary
   if (matrixChanged == true) {
     updateMatrix(); 
     matrixChanged = false; 
   }
 
-  if(displayDeath == false && displayWin == false){
-    // blink player
+  if(gameState == PLAYING){
+    // player blink
     if(millis() - playerLastBlink > PLAYER_BLINK_INTERVAL){
       playerBlink();
       playerLastBlink = millis();
@@ -168,67 +171,39 @@ void loop() {
         bombsLastBlink = millis();
       }
     }
+  }
 
-}
 
-  //daca a trecut perioada de vizibilitate intregii bombe, sterge o
+  //if visibility time of bomb is up, it's state becomes delete_bomb so that it is deleted in updateMatrix() 
   for(int i = 0; i < nrOfBombs; i++){
     if(millis() - bombs[i].bombStart > BOMBS_DISAPPEARING_INTERVAL){
-      bombs[i].state = EXPANDED_BOMB;
+      bombs[i].state = DELETE_BOMB;
       matrixChanged = true;
     }
 
-  // daca a trecut perioada de aratat o singura casuta, extinde
+  //if it is time to expand bomb, it's state becomes expand_bomb so that it is expanded in updateMatrix()
     else if(millis() - bombs[i].bombStart > BOMBS_EXPANDING_INTERVAL){
-      bombs[i].state = SHOWN_BOMB;
+      bombs[i].state = EXPAND_BOMB;
       matrixChanged = true;
     }
   }
 
-  // pune bomba noua
-  pressRead = !digitalRead(pressPin);
-  if(millis() - lastPressCheck > PRESS_DEBOUNCE_TIME){
-    lastPressCheck = millis();
+  buttonPressLogic();
 
-      if(pressRead != pressState){
-        Serial.print("changed button state\n");
-        pressState = pressRead;
-
-        if(pressState == HIGH){
-          Serial.print("HIGH button\n");
-          if(displayDeath == true){
-            Serial.println("exist death");
-            generateMap();
-            displayDeath = false;
-            matrixChanged = true;
-          }
-          else if(displayWin == true){
-            Serial.println("exist won");
-            generateMap();
-            displayWin = false;
-            matrixChanged = true;
-          }
-
-          else{
-            putBomb();
-          }
-        }
-      }
-    
-  }
-  
-  if(displayDeath == true){
-    Serial.println(millis() - showDeathStart);
+ //if the time for the death animation is up, generate next map
+  if(gameState == DEATH_ANIMATION){
     if(millis() - showDeathStart > DEFAULT_ANIMATION_DISPLAY){
       generateMap();
-      displayDeath = false;
+      gameState = PLAYING;
       matrixChanged = true;
     }
   }
-  if(displayWin == true){
+
+  //if the time for the winning animation is up, generate next map
+  if(gameState == WINNING_ANIMATION){
     if(millis() - showWinStart > DEFAULT_ANIMATION_DISPLAY){
       generateMap();
-      displayWin = false;
+      gameState = PLAYING;
       matrixChanged = true;
     }
   }
@@ -237,22 +212,23 @@ void loop() {
 
 void updateMatrix() {
 
+  // check for things to update in the matrix
   if(sizeof(bombs) > 0){
     for(int i = 0; i < nrOfBombs; i++){
       if(bombs[i].state == DROPPED_BOMB){
         matrix[bombs[i].x][bombs[i].y] = BOMB;
       }
-      if(bombs[i].state == SHOWN_BOMB){
+      if(bombs[i].state == EXPAND_BOMB){
         expandBomb(bombs[i]);
       }
-      if(bombs[i].state == EXPANDED_BOMB){
+      if(bombs[i].state == DELETE_BOMB){
         if( checkIfLost(bombs[i])){
-          displayDeath = true;
+          gameState = DEATH_ANIMATION;
           deleteBomb(bombs[i]);
         }
         else if(checkIfWon()){
           deleteBomb(bombs[i]);
-          displayWin = true;
+          gameState = WINNING_ANIMATION;
         }
         else{
           deleteBomb(bombs[i]);
@@ -261,21 +237,22 @@ void updateMatrix() {
     }
   }
   
-  if(displayDeath == true){
+
+  if(gameState == DEATH_ANIMATION){
     showSkull();
     showDeathStart = millis();
-    Serial.println("update showDeathStart");
   }
 
-  else if(displayWin == true){
+  else if(gameState == WINNING_ANIMATION){
     showWin();
     showWinStart = millis();
   }
 
-  if(displayDeath == false && displayWin == false){
+  // display visible space of the map is game is played
+  if(gameState == PLAYING){
 
-    for (int row = xBias; row < xBias + matrixSize; row++) {
-      for (int col = yBias; col < yBias + matrixSize; col++) {
+    for (int row = xBias; row < xBias + MATRIX_SIZE; row++) {
+      for (int col = yBias; col < yBias + MATRIX_SIZE; col++) {
         if(row == xPos && col ==yPos){
           lc.setLed(0, xPos - xBias, yPos - yBias, playerBlinkingState);
         }
@@ -297,12 +274,12 @@ void updateMatrix() {
 }
 
 
-
+// generates new map
 void generateMap(){
   xPos = xDefaultDistanceBetweenPosAndBias + xBias;
   yPos = yDefaultDistanceBetweenPosAndBias + yBias;
-  for(int i = 0; i < mapSize; i++){
-    for(int j = 0; j < mapSize; j++){
+  for(int i = 0; i < MAP_SIZE; i++){
+    for(int j = 0; j < MAP_SIZE; j++){
       randomNumber = random(3);
         if(randomNumber > 0){
           matrix[i][j] = WALL;
@@ -312,19 +289,20 @@ void generateMap(){
         }
     }
   }
+  // make sure that it is possible for the player to move from initial position and drop bomb
   matrix[xPos][yPos] = EMPTY_SPACE;
-  // make sure that it is possible for the player to move from initial position
-  for(int i = 0; i < nrOfDirections; i++){
+  //checks if there is already a way to move from  initial position
+  for(int i = 0; i < NR_OF_DIRECTIONS; i++){
     if(matrix[xPos + 3 * directions[i].x][yPos + 3* directions[i].y] == EMPTY_SPACE &&
     matrix[xPos + 2 * directions[i].x][yPos + 2 * directions[i].y] == EMPTY_SPACE &&
     matrix[xPos + directions[i].x][yPos + directions[i].y] == EMPTY_SPACE)
       return;
   }
 
+  // if there is no direction to move, generate one randomly
   direction randomDirection = directions[random(4)];
-  while(!(inMatrix(xPos + randomDirection.x + randomDirection.x ,yPos + randomDirection.y + randomDirection.y))){
-    randomDirection = directions[random(4)];
-  }
+  randomDirection = directions[random(4)];
+  
 
   matrix[xPos + 3 * randomDirection.x][yPos + 3 * randomDirection.y] = EMPTY_SPACE;
   matrix[xPos + 2 * randomDirection.x][yPos + 2 * randomDirection.y] = EMPTY_SPACE;
@@ -332,8 +310,44 @@ void generateMap(){
   
 }
 
+void buttonPressLogic(){
+  
+  pressRead = !digitalRead(PRESS_PIN);
+  //button press debounce
+  if(millis() - lastPressCheck > PRESS_DEBOUNCE_TIME){
+    lastPressCheck = millis();
+
+      if(pressRead != pressState){
+        pressState = pressRead;
+
+        if(pressState == HIGH){
+          // if user wants death animation to be skipped
+          if(gameState == DEATH_ANIMATION){
+            generateMap();
+            gameState = PLAYING;
+            matrixChanged = true;
+          }
+          // if user wants winning animation to be skipped
+          else if(gameState == WINNING_ANIMATION){
+            generateMap();
+            gameState = PLAYER;
+            matrixChanged = true;
+          }
+
+          //if user wants to drop bomb
+          else{
+            putBomb();
+          }
+        }
+      }
+    
+  }
+
+}
+
+//expands the bomb on the map
 void expandBomb(bomb currentBomb){  
-  for(int currentCoordonates = 0; currentCoordonates < nrOfExtendedBombCoordones; currentCoordonates++){
+  for(int currentCoordonates = 0; currentCoordonates < NR_OF_EXTENDED_BOMB_COORDONATES; currentCoordonates++){
     int i = extendedBombCoordonates[currentCoordonates].x + currentBomb.x;
     int j = extendedBombCoordonates[currentCoordonates].y + currentBomb.y;
       if(inMatrix(i, j)){
@@ -342,7 +356,7 @@ void expandBomb(bomb currentBomb){
     }
   }
 
-
+//deletes bomb on the matrix
 void deleteBomb(bomb currentBomb) {
 
   for (int i = 0; i < nrOfBombs - 1; i++) {
@@ -351,7 +365,7 @@ void deleteBomb(bomb currentBomb) {
   nrOfBombs--;
   bombs = (bomb*)realloc(bombs, nrOfBombs * sizeof(bomb));
 
-  for(int currentCoordonates = 0; currentCoordonates < nrOfExtendedBombCoordones; currentCoordonates++){
+  for(int currentCoordonates = 0; currentCoordonates < NR_OF_EXTENDED_BOMB_COORDONATES; currentCoordonates++){
     int i = extendedBombCoordonates[currentCoordonates].x + currentBomb.x;
     int j = extendedBombCoordonates[currentCoordonates].y + currentBomb.y;
       if(inMatrix(i, j)){
@@ -361,9 +375,10 @@ void deleteBomb(bomb currentBomb) {
 
 }
 
+//checks if user was killed by the exploding bomb
 bool checkIfLost(bomb currentBomb){
   
-  for(int currentCoordonates = 0; currentCoordonates < nrOfExtendedBombCoordones; currentCoordonates++){
+  for(int currentCoordonates = 0; currentCoordonates < NR_OF_EXTENDED_BOMB_COORDONATES; currentCoordonates++){
     int i = extendedBombCoordonates[currentCoordonates].x + currentBomb.x;
     int j = extendedBombCoordonates[currentCoordonates].y + currentBomb.y;
       if(xPos == i && yPos == j){
@@ -374,40 +389,39 @@ bool checkIfLost(bomb currentBomb){
 }
 
 
-// Function to read joystick input and update the position of the LED
+//read joystick input and update the position of the player
 void updatePositions() {
-  int xValue = analogRead(yPin);
-  int yValue = analogRead(xPin);
-  // Store the last positions of the LED
+  int xValue = analogRead(Y_PIN);
+  int yValue = analogRead(X_PIN);
   xLastPos = xPos;
   yLastPos = yPos;
   
-  // Update xPos based on joystick movement (X-axis)
-  if (xValue > maxThreshold) {
-    if (xPos < mapSize - 1) {
+  // update xPos based on joystick movement (X-axis)
+  if (xValue > MAX_THRESHOLD) {
+    if (xPos < MAP_SIZE - 1) {
       xPos++;
     } 
   }
-  if (xValue < minThreshold) {
+  if (xValue < MIN_THRESHOLD) {
     if (xPos > 0) {
       xPos--;
     }
   }
 
-  // Update yPos based on joystick movement (Y-axis)
-  if (yValue > maxThreshold) {
-    if (yPos < mapSize - 1) {
+  // update yPos based on joystick movement (Y-axis)
+  if (yValue > MAX_THRESHOLD) {
+    if (yPos < MAP_SIZE - 1) {
       yPos++;
     } 
   }
-  if (yValue < minThreshold) {
+  if (yValue < MIN_THRESHOLD) {
     if (yPos > 0) {
       yPos--;
     }
   }
- // Check if the position has changed and update the matrix if necessary
+ // check if the position has changed and update the matrix if necessary
   if (xPos != xLastPos || yPos != yLastPos) {
-    // sa nu se miste pe diagonala
+    // if both the X-axis and Y-axis were changed and chenge only the one with the biggest movement on the axis
     if (xPos != xLastPos && yPos != yLastPos){
       if(module(DEFAULT_JOYSTICK - xValue) > module(DEFAULT_JOYSTICK - yValue)){
         yPos = yLastPos;
@@ -416,30 +430,30 @@ void updatePositions() {
         xPos = xLastPos;
       } 
     }
-    // sa nu treci prin perete
+    //check that the next space is empty
     if(matrix[xPos][yPos] == EMPTY_SPACE){
-      // daca trebuie sa se miste harta
+      //move the "view" of the map with the player movement, unless the map is at its end
       xBias = xPos - xDefaultDistanceBetweenPosAndBias;
       yBias = yPos - yDefaultDistanceBetweenPosAndBias;
 
       if(xBias < 0){
         xBias = 0;
       }
-      if(xBias + matrixSize > mapSize){
-        xBias = mapSize - matrixSize;
+      if(xBias + MATRIX_SIZE > MAP_SIZE){
+        xBias = MAP_SIZE - MATRIX_SIZE;
       }
 
       if(yBias < 0){
         yBias = 0;
       }
-      if(yBias + matrixSize > mapSize){
-        yBias = mapSize - matrixSize;
+      if(yBias + MATRIX_SIZE > MAP_SIZE){
+        yBias = MAP_SIZE - MATRIX_SIZE;
       }
-
 
       matrixChanged = true;
 
     }
+    // if the next space is not empty, do not move
     else{
       xPos = xLastPos;
       yPos = yLastPos;
@@ -447,13 +461,15 @@ void updatePositions() {
   }
 }
 
-bool inMatrix(byte i, byte j){
-  if(i < 0 || i > mapSize - 1 || j < 0 || j > mapSize - 1){
+// checks if the coordonates are within the map
+bool inMatrix(int i,int j){
+  if(i < 0 || i > MAP_SIZE - 1 || j < 0 || j > MAP_SIZE - 1){
     return false;
   }
   return true;
 }
 
+// allocates memory for a bomb and makes the bomb visible
 void putBomb(){
   nrOfBombs++;
   bombs = (bomb*) realloc(bombs, nrOfBombs * sizeof(bomb));
@@ -467,39 +483,40 @@ void putBomb(){
 
 }
 
-
+//changes the state of the bomb blink
 void bombsBlink(){
   bombsBlinkingState = !bombsBlinkingState;
   matrixChanged = true;
 }
 
+//changes the state of the player blink
 void playerBlink(){
   playerBlinkingState = !playerBlinkingState;
   matrixChanged = true;
 }
 
-
+//shows dying animation
 void showSkull(){
-  for (int row = 0; row < matrixSize; row++) {
-    for (int col = 0; col < matrixSize; col++) {
+  for (int row = 0; row < MATRIX_SIZE; row++) {
+    for (int col = 0; col < MATRIX_SIZE; col++) {
       lc.setLed(0, row, col, skullAnimation[row][col]);
     }
   }
 }
 
-
+//shows winning animation
 void showWin(){
-  for (int row = 0; row < matrixSize; row++) {
-    for (int col = 0; col < matrixSize; col++) {
+  for (int row = 0; row < MATRIX_SIZE; row++) {
+    for (int col = 0; col < MATRIX_SIZE; col++) {
       lc.setLed(0, row, col, winAnimation[row][col]);
     }
   }
 }
 
-
+//checks if there are walls left
 bool checkIfWon(){
-  for(int i = 0; i < mapSize; i++){
-    for(int j = 0; j < mapSize; j++){
+  for(int i = 0; i < MAP_SIZE; i++){
+    for(int j = 0; j < MAP_SIZE; j++){
       if(matrix[i][j] == WALL){
         return false;
       }
@@ -509,6 +526,7 @@ bool checkIfWon(){
     
 }
 
+//arithmethic module operation
 int module(int number){
   if(number < 0){
     return number * (-1);
